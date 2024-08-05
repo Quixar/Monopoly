cb47dc59befe03e13fc7858a89a14a90d3069309
Elina <elinvar230507@gmail.com>
2024-08-05 17:50:15 +0300

    Field

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace Monopoly_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {                      
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.SetWindowPosition(Console.WindowLeft, Console.WindowTop);
+
+        }
+    }
+}
