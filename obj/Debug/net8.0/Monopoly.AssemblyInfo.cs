8872548ed0108f4a44522d448f9b71a7fbf0b34b
Quixar <evgenmart01@gmail.com>
2024-10-17 11:21:33 +0300

    final version

--- obj/Debug/net8.0/Monopoly.AssemblyInfo.cs
+++ obj/Debug/net8.0/Monopoly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monopoly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bc5e6b9a65b6ab16964146adbe11633f4c91e25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c7d1cfa2afa3ff375b3d8e275debab82640e4c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monopoly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monopoly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
